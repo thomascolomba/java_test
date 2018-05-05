c46355a91bfd847377338bab2178480ceac0e618
unknown <thomas.colomba30@gmail.com>
2018-05-05 19:37:57 +0100

    second commit

---
+++ src/main/java/java_test/Multiply2.java
@@ -0,0 +1,7 @@
+package java_test;
+
+public class Multiply2 {
+	public static int multiply2(int value) {
+		return 2*value;
+	}
+}
---
+++ src/test/java/java_test/TestMultiply2.java
@@ -0,0 +1,20 @@
+package java_test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestMultiply2 {
+	@Test
+	public void test1() {
+		assertEquals(Multiply2.multiply2(1), 2);
+	}
+
+	@Test
+	public void test0() {
+		assertEquals(Multiply2.multiply2(0), 0);
+	}
+
+	@Test
+	public void testMinus1() {
+		assertEquals(Multiply2.multiply2(-1), -2);
+	}
+}
